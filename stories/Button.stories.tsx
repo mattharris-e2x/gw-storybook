d4ccf1287ca3f12ae8b77539aed12cd354fde68e
yesitsmattharris <yesitsmattharris@gmail.com>
2022-03-24 12:15:06 +0000

    test storybook with github ci

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -23,6 +23,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
+  backgroundColor: 'yellow',
   label: 'Button primary',
 };
 
